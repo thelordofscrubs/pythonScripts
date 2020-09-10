6429f74237e377d32d0e5fa6892505a4548a037a
thelordofscrubs <ekaituri@gmail.com>
2020-09-10 16:04:03 -0500

    some work on the request thing, still dont really know what to do with it

--- rlpathfinderproto.py
+++ rlpathfinderproto.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 def calcDist(loc1,loc2):
     return math.sqrt((loc2.x-loc1.x)**2+(loc2.y-loc1.y)**2)
@@ -14,8 +15,14 @@ class Location:
         self.locid = self.locamt
         self.locamt += 1
         self.connections = []
+        self.requests = []
     def connect(self, newLoc):
         self.connections.append(Connection(self, newLoc))
+    def addRequest(self, req):
+        self.requests.append(req)
+    def addRandReq(self):
+        global mapLocations
+        self.requests.append(Request(self.locid,random.choice(mapLocations),random.randint(10,72),random.randint(500,5000),random.randint(100,10000),random.randint(1,4)))
 
 class Connection:
     conamt = 0 #total amount of connections, used to increment conid so there are no duplicates
@@ -29,6 +36,8 @@ class Connection:
 class Request:
     reqamt = 0
     def __init__(self,fro,to,within,pay,weight,space):
+        self.reqid = self.reqamt
+        self.reqamt += 1
         self.fromPlace = fro
         self.toPlace = to
         self.timeLimit = within
